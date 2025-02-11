38fdb0dc75f56c15c893ca72fa21c2fc0dfd93f5
MONKEY-D <118673486+MONKEY-D@users.noreply.github.com>
2025-02-11 16:22:05 +0530

    nextConfig file remanes to mjs and some changes inside

---
+++ next.config.mjs
@@ -0,0 +1,19 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    staleTimes: {
+      dynamic: 30,
+    }
+  }
+}
+
+export default nextConfig;
