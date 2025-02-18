bd5b31a008272eaf8893f62cd87cbf3b77e88ec1
MONKEY-D <118673486+MONKEY-D@users.noreply.github.com>
2025-02-18 10:14:46 +0530

    implemented optimistic updates to follow button

--- src/components/FollowButton.tsx
+++ src/components/FollowButton.tsx
@@ -3,7 +3,7 @@
 import { useToast } from "@/hooks/use-toast";
 import useFolloweInfo from "@/hooks/useFollowerInfo";
 import { FollowerInfo } from "@/lib/types";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { QueryKey, useMutation, useQueryClient } from "@tanstack/react-query";
 import { Button } from "./ui/button";
 import kyInstance from "@/lib/ky";
 
@@ -22,16 +22,40 @@ export default function FollowButton({
 
   const { data } = useFolloweInfo(userId, initialState);
 
+  const queryKey: QueryKey = ["follower-info", userId];
+
   const { mutate } = useMutation({
     mutationFn: () =>
       data.isFollowedByUser
         ? kyInstance.delete(`/api/users/${userId}/followers`)
         : kyInstance.post(`/api/users/${userId}/followers`),
+    onMutate: async () => {
+      await queryClient.cancelQueries({ queryKey });
+
+      const previousState = queryClient.getQueryData<FollowerInfo>(queryKey);
+
+      queryClient.setQueryData<FollowerInfo>(queryKey, () => ({
+        followers:
+          (previousState?.followers || 0) +
+          (previousState?.isFollowedByUser ? -1 : 1),
+        isFollowedByUser: !previousState?.isFollowedByUser,
+      }));
+
+      return { previousState };
+    },
+    onError(error, variables, context) {
+      queryClient.setQueryData(queryKey, context?.previousState);
+      console.error(error);
+      toast({
+        variant: "destructive",
+        description: "Something went wrong. Plese try again.",
+      });
+    },
   });
 
   return (
     <Button
-      variant={data.isFollowedByUser ? "secondry" : "default"}
+      variant={data.isFollowedByUser ? "secondary" : "default"}
       onClick={() => mutate()}
     >
       {data.isFollowedByUser ? "Unfollow" : "Follow"}
